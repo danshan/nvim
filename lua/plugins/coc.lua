79f66dcc65ec4df34a060ab984839114ccc4d15b
Honghao SHAN 单弘昊 <honghao.shan@nio.com>
2023-04-07 13:03:10 +0800

    feature remove plugin/packer_compiled.lua

---
+++ lua/plugins/coc.lua
@@ -0,0 +1,12 @@
+vim.g.coc_global_extensions = {
+  'coc-lua',
+  'coc-json',
+  'coc-groovy',
+  'coc-yaml',
+  'coc-tsserver',
+  'coc-sh',
+  'coc-marketplace',
+  'coc-pyright',
+  'coc-vimlsp',
+}
+
